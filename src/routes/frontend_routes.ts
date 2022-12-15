3661fb26f6ecc80941f6f20f31977f3d91b427a2
witaway <witaway33@gmail.com>
2022-12-16 01:51:57 +0300

    Basic auth routing

--- src/routes/frontend_routes.ts
+++ src/routes/frontend_routes.ts
@@ -1,22 +1,76 @@
 //const Router = require('express').Router;
 import { Router } from 'express';
+import AuthService from '../services/auth';
+import {
+	PasswordNotMatchException,
+	UsernameNotFoundException,
+} from '../classes/errors';
 
 const frontendRouter = Router();
 
-//const auth = require('../middlewares/auth_frontend');
-//router.use(auth);
+import auth from '../middlewares/auth';
 
-// Respond with game page
+frontendRouter.use(auth);
+
 frontendRouter.get('/', (req, res) => {
-	res.send('Hello!');
-	// if (req.user) res.render('hub', {});
-	// else res.render('login');
+	console.log(req.user);
+	if (!req.user) {
+		res.redirect('/login');
+	} else {
+		res.send('Hi!');
+	}
 });
 
-// Respond with game page
-frontendRouter.get('/game', (req, res, next) => {
-	if (req.user) res.render('game');
-	else next();
+frontendRouter.get('/login', (req, res) => {
+	if (req.user) {
+		res.redirect('/');
+		return;
+	}
+	res.render('login', {});
+});
+
+frontendRouter.post('/login', async (req, res) => {
+	if (!req.body.username || !req.body.password) {
+		res.render('login', {
+			username: req.body.username,
+			password: req.body.password,
+			errorMessage: 'Необходимо заполнить все поля',
+			focus: !req.body.username ? 'username' : 'password',
+		});
+		return;
+	}
+
+	try {
+		const token = await AuthService.loginWithUsernameAndPassword(
+			req.body.username,
+			req.body.password,
+		);
+
+		const sec = 1000;
+		const min = 60 * sec;
+		const hour = 60 * min;
+
+		res.cookie('jwt', token, {
+			maxAge: 2 * hour,
+		});
+		res.redirect('/');
+	} catch (error) {
+		if (error instanceof UsernameNotFoundException) {
+			res.render('login', {
+				username: req.body.username,
+				password: req.body.password,
+				errorMessage: `Пользователь ${req.body.username} не зарегистрирован.<br>Проверьте написание`,
+			});
+		}
+		if (error instanceof PasswordNotMatchException) {
+			res.render('login', {
+				username: req.body.username,
+				password: req.body.password,
+				errorMessage: `Пароль введён неверно`,
+				focus: 'password',
+			});
+		}
+	}
 });
 
 frontendRouter.get('*', (req, res) => {
