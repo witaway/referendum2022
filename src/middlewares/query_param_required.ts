3afb2f794753033e0a5bc1a2518b3721c0d2ac3e
witaway <witaway33@gmail.com>
2022-12-16 05:29:48 +0300

    Handle missing get parameters

---
+++ src/middlewares/query_param_required.ts
@@ -0,0 +1,15 @@
+const requiredQueryParams = (requiredQueryParameters: [string]) => {
+	return (req: Express.Request, res: Express.Response, next: any) => {
+		for (const param of requiredQueryParameters) {
+			if (!req.query[param]) {
+				res.render('error', {
+					message: `Отсутствует обязательный параметр ${param}`,
+				});
+				return;
+			}
+		}
+		next();
+	};
+};
+
+export default requiredQueryParams;
---
+++ src/routes/frontend.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import auth from '@middlewares/auth';
+
+import loginRouter from './auth/login';
+import logoutRouter from './auth/logout';
+import toursRouter from './tours';
+import placesRouter from './places';
+import requiredQueryParams from '@middlewares/query_param_required';
+
+const router = Router();
+
+router.use(auth);
+
+router.use('/login', loginRouter);
+router.use('/logout', logoutRouter);
+router.use('/', toursRouter);
+router.use('/places', requiredQueryParams(['tour_id']), placesRouter);
+
+router.get('*', (req, res) => {
+	res.status(404);
+	res.send('This page does not exist');
+});
+
+export default router;
