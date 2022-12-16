d6cc39c35a4012b8c148ca777f44409f42097db5
witaway <witaway33@gmail.com>
2022-12-16 05:32:32 +0300

    Tour page with mock data

---
+++ src/routes/tours.ts
@@ -0,0 +1,36 @@
+import { Router } from 'express';
+import { Role } from '@prisma/client';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+	if (!req.user) {
+		res.redirect('/login');
+	} else {
+		const links_to = req.user.role === Role.EDITOR ? 'places' : 'results';
+		res.render('tours', {
+			date_formatted: '02:35:30 16/12/2022',
+			links_to: links_to,
+			tours: [
+				{
+					id: 1,
+					number: 1,
+					protocols: {
+						processed: 9,
+						total: 24,
+					},
+				},
+				{
+					id: 2,
+					number: 2,
+					protocols: {
+						processed: 0,
+						total: 24,
+					},
+				},
+			],
+		});
+	}
+});
+
+export default router;
