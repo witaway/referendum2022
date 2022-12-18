8759c539f0245afce79515955bab1345dad0620e
witaway <witaway33@gmail.com>
2022-12-19 00:33:18 +0300

    Handlebars helpers in separate file

---
+++ src/helpers/handlebars-helpers.ts
@@ -0,0 +1,19 @@
+const handlebarsHelpers: any = {
+	ternary: function (cond: any, v1: any, v2: any) {
+		return cond ? v1 : v2;
+	},
+	plusOne: function (value: any) {
+		return value + 1;
+	},
+	switch: function (value: any, options: any) {
+		this.switch_value = value;
+		return options.fn(this);
+	},
+	case: function (value: any, options: any) {
+		if (value == this.switch_value) {
+			return options.fn(this);
+		}
+	},
+};
+
+export default handlebarsHelpers;
--- src/loaders/express-modules-loader.ts
+++ src/loaders/express-modules-loader.ts
@@ -11,29 +11,14 @@ import passport from 'passport';
 // https://stackoverflow.com/questions/55504066/how-to-gracefully-handle-promise-rejection-in-express
 import 'express-async-errors';
 import * as process from 'process';
+import handlebarsHelpers from '@helpers/handlebars-helpers';
 
 const initExpressModules = (server: Express) => {
 	server.engine(
 		'handlebars',
 		engine({
 			defaultLayout: false,
-			helpers: {
-				ternary: function (cond: any, v1: any, v2: any) {
-					return cond ? v1 : v2;
-				},
-				plusOne: function (value: any) {
-					return value + 1;
-				},
-				switch: function (value: any, options: any) {
-					this.switch_value = value;
-					return options.fn(this);
-				},
-				case: function (value: any, options: any) {
-					if (value == this.switch_value) {
-						return options.fn(this);
-					}
-				},
-			},
+			helpers: handlebarsHelpers,
 		}),
 	);
 
@@ -44,7 +29,6 @@ const initExpressModules = (server: Express) => {
 	server.use(bodyParser.urlencoded({ extended: true }));
 	server.use(cookieParser(process.env['COOKIE_SECRET']));
 
-	// TODO: Add passport and uncomment
 	server.use(passport.initialize());
 };
 
